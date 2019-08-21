8ffb6b0b2a9f7a06880561cb8679660ec9b9a286
Artenes Nogueira <artenesama@gmail.com>
2019-08-21 19:28:51 -0400

    Adds endpoints to get all users and create users

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 const users = ["Juliet", "Reachel", "Monique"];
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
 
 server.get("/users/:index", (req, res) => {
   const { index } = req.params;
 
   return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
 });
 
 server.listen(3000);
